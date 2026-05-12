2670322a5b054feb46a3a750c59777bc14d89656
Nisa <nisa8610@gmail.com>
2026-05-12 10:32:28 +0530

    Time: 80 ms (8.27%), Space: 57.9 MB (42.86%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function (nums) {
+    nums.sort((a, b) => a - b);
+    let sum = nums[nums.length - 2] * nums[nums.length - 1] - nums[0] * nums[1];
+    return sum;
+};
