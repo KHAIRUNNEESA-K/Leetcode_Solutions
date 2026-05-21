9e52d9bef03fc24482b6445882dfc0ced14d0721
Nisa <nisa8610@gmail.com>
2026-05-21 10:41:29 +0530

    Time: 0 ms (100%), Space: 57.2 MB (27.15%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function (nums) {
+    let neg = 0
+    let pos = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            pos++;
+        }
+        if (nums[i] < 0) {
+            neg++;
+        }
+    }
+    let maximumCount = Math.max(pos, neg);
+    return maximumCount;
+};
